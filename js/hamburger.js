67d4bb9916cc77704f6b92470f7f75d982e3ae0a
kurokinami0702 <96125115+kurokinami0702@users.noreply.github.com>
2026-06-01 22:17:06 +0900

    Add files via upload

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,12 +2,16 @@ window.onload = function(){
     var nav = document.getElementById('nav-wrapper');
     var hamburger = document.getElementById('js-hamburger');
     var blackbg = document.getElementById('js-black-bg');
+    var link = document.getElementById('js-link');
 
     hamburger.addEventListener('click', function () {
         nav.classList.toggle('open');
     });
 
     blackbg.addEventListener('click',function(){
+        nav.classList.remove('open')
+    })
+    link.addEventListener('click',function(){
         nav.classList.remove('open')
     })
 }
